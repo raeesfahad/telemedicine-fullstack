b5f33946f1f1cf999f21f936ebe558fe095278ab
raeesfahad <raeesfahad024@gmail.com>
2023-02-18 13:51:14 +0000

    updated for doctors page

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -10,17 +10,11 @@
 				<div
 					class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"
 				>
-					<svg
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						class="w-6 h-6"
-						viewBox="0 0 24 24"
-					>
-						<path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+				<svg width="41" height="41" viewBox="0 0 20 20" fill="#264d6e" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.667 5.5c-.645 0-1.167.522-1.167 1.167v6.666c0 .645.522 1.167 1.167 1.167h6.666c.645 0 1.167-.522 1.167-1.167V6.667c0-.645-.522-1.167-1.167-1.167H4.667zM2.5 6.667C2.5 5.47 3.47 4.5 4.667 4.5h6.666c1.197 0 2.167.97 2.167 2.167v6.666c0 1.197-.97 2.167-2.167 2.167H4.667A2.167 2.167 0 012.5 13.333V6.667z" clip-rule="evenodd"/>
+					<path fill-rule="evenodd" d="M13.25 7.65l2.768-1.605a.318.318 0 01.482.263v7.384c0 .228-.26.393-.482.264l-2.767-1.605-.502.865 2.767 1.605c.859.498 1.984-.095 1.984-1.129V6.308c0-1.033-1.125-1.626-1.984-1.128L12.75 6.785l.502.865z" clip-rule="evenodd"/>
 					</svg>
+				
+				
 				</div>
 				<div class="flex-grow pl-6">
 					<h2 class="text-gray-900 text-lg title-font font-medium mb-2 dark:text-neutral-100">
@@ -37,18 +31,8 @@
 				<div
 					class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"
 				>
-					<svg
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						class="w-6 h-6"
-						viewBox="0 0 24 24"
-					>
-						<circle cx="6" cy="6" r="3" />
-						<circle cx="6" cy="18" r="3" />
-						<path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
+				<svg width="42" height="42" viewBox="0 0 20 20" fill="#264d6e" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 3h8a2 2 0 012 2v10a2 2 0 01-2 2H6a2 2 0 01-2-2V5a2 2 0 012-2zm0 1a1 1 0 00-1 1v10a1 1 0 001 1h8a1 1 0 001-1V5a1 1 0 00-1-1H6z" clip-rule="evenodd"/>
+					<path fill-rule="evenodd" d="M6.5 14a.5.5 0 01.5-.5h3a.5.5 0 010 1H7a.5.5 0 01-.5-.5zm0-2a.5.5 0 01.5-.5h6a.5.5 0 010 1H7a.5.5 0 01-.5-.5zm0-2a.5.5 0 01.5-.5h6a.5.5 0 010 1H7a.5.5 0 01-.5-.5zm0-2a.5.5 0 01.5-.5h6a.5.5 0 010 1H7a.5.5 0 01-.5-.5zm0-2a.5.5 0 01.5-.5h6a.5.5 0 010 1H7a.5.5 0 01-.5-.5z" clip-rule="evenodd"/>
 					</svg>
 				</div>
 				<div class="flex-grow pl-6">
@@ -66,33 +50,24 @@
 				<div
 					class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"
 				>
-					<svg
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						class="w-6 h-6"
-						viewBox="0 0 24 24"
-					>
-						<path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-						<circle cx="12" cy="7" r="4" />
-					</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-activity-heartbeat" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"/> <path d="M3 12h4.5l1.5 -6l4 12l2 -9l1.5 3h4.5" /> </svg>
 				</div>
 				<div class="flex-grow pl-6">
 					<h2 class="text-gray-900 text-lg title-font font-medium mb-2 dark:text-neutral-100">
 						E-Labs
 					</h2>
 					<p class="leading-relaxed text-base dark:text-neutral-100">
-						We offer a fast, convenient, and reliable way to get
-						the lab tests you need. simply vist our partner lab and submit a sample. receive your results securely online. we make it easy
-						to monitor your health and get the answers you need."
+						We offer a fast, convenient, and reliable way to get the lab tests you need. simply vist
+						our partner lab and submit a sample. receive your results securely online. we make it
+						easy to monitor your health and get the answers you need."
 					</p>
 				</div>
-        
 			</div>
-    <button class=" mx-20 mt-15 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Join RemoteCare</button>
-
-  </div>
+		</div>
 	</div>
 </div>
+
+<button
+	class=" mx-20 mt-15 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+	>Join RemoteCare</button
+>
